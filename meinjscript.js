ee224916e4bec1b10e4566798f33c2c29ad61cb6
dkuhne <daniel.kuhne@s1nn.de>
2014-03-27 09:41:05 +0100

    Use fadeTo animation for result and count children

--- meinjscript.js
+++ meinjscript.js
@@ -3,10 +3,15 @@
 /* Wenn der Button angelickt wird dann wird in einer Schleife
  *  der Text innerhalb des DIV angehängt. */
 function clickhandler() { 
+  $("#ergebnis").css("opacity","0");
+  $("#ergebnisCounter").css("visibility","hidden");
   for(var i=0; i<100; i++) {
       $("#ergebnis").append("Hier kannste scrollen #"+i+"<br/>");
    }
-   $("#ergebnis").css("visibility", "visible");
+   $("#ergebnis").fadeTo( 1000, 1.0, function() {
+     $("#ergebnisCounter").text($("#ergebnis").children().length + " Elemente");
+     $("#ergebnisCounter").css("visibility","visible");
+   })
 }
 
 $(document).ready(function(){
